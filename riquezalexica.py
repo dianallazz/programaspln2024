befe4471831faecb7fc8186c31d3e69dfb993fdd
dianallazz <dllamas@ucol.mx>
2024-03-07 20:20:11 -0600

    actualizacion de archivos

---
+++ riquezalexica.py
@@ -0,0 +1,18 @@
+
+import nltk
+
+nltk.download('punkt')
+carpeta_nombre = r"C:/Users/diana/OneDrive/Documentos/Escuela/6B/programaspln2024/"
+archivo_nombre="texto8.txt"
+with open(carpeta_nombre + archivo_nombre, "r") as archivo:
+
+    texto=archivo.read()
+tokens=nltk.word_tokenize(texto,"spanish")
+for token in tokens:
+    print(token)
+
+tokens_conjunto=set(tokens)
+palabras_totales=len(tokens)
+palabras_diferentes=len(tokens_conjunto)
+riquiza_lexica=palabras_diferentes/palabras_totales
+print("riqueza lexica: ",riquiza_lexica)
